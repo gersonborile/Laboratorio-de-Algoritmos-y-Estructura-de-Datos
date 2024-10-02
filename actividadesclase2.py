ceb4c0acbb9eac0cc2115a7be046cd592ca9c894
gersonborile <borilegerson@gmail.com>
2024-10-02 20:01:47 -0300

    Add files via upload

---
+++ actividadesclase2.py
@@ -0,0 +1,104 @@
+#ejercicio 1
+nombre = "Tony Soprano"
+edad = 51
+
+#ejercicio 2
+
+nombre = "Julia"
+apellido = "Roberts"
+nombre_completo = (nombre+" "+apellido)
+print(nombre_completo)
+
+#ejercicio 3
+
+curso = "Python"
+print("Estas tomando un curso de "+curso)
+
+#ejercicio 4
+
+num_entero = 51
+print(type(num_entero))
+
+#ejercicio 5
+
+num_decimal = 10.10
+print(type(num_decimal))
+
+#ejercicio 6
+
+num1 = 7.5
+num2 = 2.5
+suma=(num1+num1)
+print(type(suma))
+
+#ejercicio 7
+
+num1 = 7.5
+num_int = int(num1)  
+print(type(num_int))  
+
+
+#ejercicio 8
+
+num2 = 10
+num_float = float(num2)  
+print(type(num_float))    
+
+#ejercicio 9
+
+num1="7.5"
+num2="10"
+print(float(num1) + int(num2))
+
+#ejercicio 10
+
+nombre_asociado = "Jesse Pinkman"
+numero_asociado = 399058
+
+print(f"Estimado/a {nombre_asociado}, su número de asociado es: {numero_asociado}")
+
+#ejercicio 11
+
+puntos_nuevos = 350
+puntos_totales = 1225
+
+print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_totales} puntos")
+
+#ejercicio 12
+
+puntos_nuevos = 350
+puntos_anteriores= 1225
+
+print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_anteriores+puntos_nuevos} puntos")
+
+#ejercicio 13
+
+num1= 874
+num2= 27
+print(num1//num2)
+
+#ejercicio 14
+
+num1=456
+num2=33
+print(num1%num2)
+
+#ejercicio 15
+
+num1=783
+print(num1**0.5)
+
+#ejercicio 16
+
+print(round(10/3,2))
+
+#ejercicio 17
+
+print(round(10.676767 ))
+
+#ejercicio 18
+
+num1=5
+print(num1**0.5)
+resultado=num1**0.5
+print(round(resultado,4))
